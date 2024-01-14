cd2ed0efa17ad914d363d86a40c29a12d60ea038
Jan Havlat <jan.havlath@gmail.com>
2024-01-15 00:33:11 +0100

    Adding my TRVL-website project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavigationBar from './components/navigation-bar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/home';
+import Services from './components/pages/services';
+import Products from './components/pages/products';
+import SignUp from './components/pages/sign-up';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/services' Component={Services} />
+          <Route path='/products' Component={Products} />
+          <Route path='/sign-up' Component={SignUp} />
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 
