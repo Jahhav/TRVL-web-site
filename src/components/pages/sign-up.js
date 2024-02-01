ecd4a8e9d350d2ece4b15e75036f1bc05f2296d1
Jan Havlat <jan.havlath@gmail.com>
2024-02-01 02:22:05 +0100

    added SIGN UP page where are 2 formulas for sign up and for register (not working yet but will implement next some document forwadrding). Now First test on mobiles

--- src/components/pages/sign-up.js
+++ src/components/pages/sign-up.js
@@ -1,6 +1,55 @@
 import React from "react";
 import '../../App.css'
+import './sign-up.css'
 
 export default function SignUp() {
-    return <h1 className="sign-up">SIGN UP</h1>
+
+    const handleSubmit = () => {
+
+    }
+
+
+    return (
+        <div className="sign-up-body">
+
+            <div className="signup-card first-card">
+
+                <div className="h1-container">
+                    <h1 className="sign-up">SIGN IN</h1>
+                </div>
+
+                <div className="form-container">
+                    <form>
+                        <input type="email" placeholder="email"></input>
+                        <input type="password" placeholder="password"></input>
+                        <div className="button-placeholder">
+                            <button type="submit" className="button-button" onClick={handleSubmit}>SUBMIT</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+            <div className="signup-card second-card">
+
+                <div className="h1-container">
+                    <h1 className="sign-up">REGISTER</h1>
+                </div>
+
+                <div className="form-container">
+                    <form>
+                        <input type="text" placeholder="username"></input>
+                        <input type="email" placeholder="email"></input>
+                        <input type="password" placeholder="password"></input>
+                        <div className="button-placeholder">
+                            <button type="submit" className="button-button" onClick={handleSubmit}>SUBMIT</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+    )
+
 }
